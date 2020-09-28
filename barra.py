bd1164228256d167b374471e4c19130303d25309
creperezf <69158551+creperezf@users.noreply.github.com>
2020-09-28 10:39:53 -0300

    Update barra.py
    codigo barra completo, con todos los cambios

--- barra.py
+++ barra.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 g = 9.81 #kg*m/s^2
-
+phi = 3.14
 
 class Barra(object):
 
@@ -18,19 +18,26 @@ class Barra(object):
 
 	def obtener_conectividad(self):
 		"""Implementar"""
-		return 
+		return [self.ni,self.nj]
 
 	def calcular_area(self):
-		"""Implementar"""
-		return 
+		r = self.R - self.t
+		area = phi*((self.R**2)-(r**2))
+		return area 
 
 	def calcular_largo(self, reticulado):
 		"""Devuelve el largo de la barra. 
 		xi : Arreglo numpy de dimenson (3,) con coordenadas del nodo i
 		xj : Arreglo numpy de dimenson (3,) con coordenadas del nodo j
 		"""
-		"""Implementar"""
-		return 
+		"""saco las coordenadas (x,y,z) de los nodos"""
+		n_i = reticulado.obtener_coordenada_nodal(self.ni)
+		n_j = reticulado.obtener_coordenada_nodal(self.nj)
+		x = abs(n_i[0]-n_j[0])
+		y = abs(n_i[1]-n_j[1])
+		z = abs(n_i[2]-n_j[2])
+
+		L = (x**2 + y**2)**(0.5)
 
 	def calcular_peso(self, reticulado):
 		"""Devuelve el largo de la barra. 
@@ -38,8 +45,11 @@ class Barra(object):
 		xj : Arreglo numpy de dimenson (3,) con coordenadas del nodo j
 		"""
 		"""Implementar"""
-		return 
+		a = self.calcular_area()
+		largo = self.calcular_largo(reticulado)
 
+		peso = (a*largo)*self.ρ*g
+		return peso
 
 
 
